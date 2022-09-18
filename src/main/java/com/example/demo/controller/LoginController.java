6718a15ae00251bf3e692bb2d6c13635536f725c
LAPTOP-JG661I0R\tomoyuki <hzd02765g@gmail.com>
2022-09-18 12:06:19 +0900

    7-1.レイアウトの実装

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,11 +2,19 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+	
+	/** ログイン画面を表示 */
 	@GetMapping("/login")
 	public String getLogin() {
 		return "login/login";
+	}
+	
+	@PostMapping("/login")
+	public String postLogout() {
+		return "redirect:/user/list";
 	}
 }
