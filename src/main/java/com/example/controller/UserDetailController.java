72e86ca0e66a45fd1b76e09699861dadc8565331
LAPTOP-JG661I0R\tomoyuki <hzd02765g@gmail.com>
2022-09-18 17:27:40 +0900

    8-4-2.トランザクションの実装

--- src/main/java/com/example/controller/UserDetailController.java
+++ src/main/java/com/example/controller/UserDetailController.java
@@ -13,8 +13,13 @@ import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.UserService;
 import com.example.form.UserDetailForm;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserDetailController {
 
 	@Autowired
@@ -44,7 +49,14 @@ public class UserDetailController {
 	
 	@PostMapping(value="/detail", params="update")
 	public String updateUser(UserDetailForm form, Model model) {
-		userService.updateUserOne(form.getUserId(), form.getPassword(), form.getUserName());
+		
+		try {
+			// ユーザーを更新
+			userService.updateUserOne(form.getUserId(), form.getPassword(), form.getUserName());
+		} catch (Exception e) {
+			log.error("ユーザー更新でエラー", e);
+		}
+		
 		
 		return "redirect:/user/list";
 	}
