8fd4e3f50580601f04687431f4b5fbbd524437cb
LAPTOP-JG661I0R\tomoyuki <hzd02765g@gmail.com>
2022-09-18 16:55:12 +0900

    8-3-2.ネストしたオブジェクトへのマッピング

--- src/main/java/com/example/form/UserDetailForm.java
+++ src/main/java/com/example/form/UserDetailForm.java
@@ -2,6 +2,8 @@ package com.example.form;
 
 import java.util.Date;
 
+import com.example.domain.user.model.Department;
+
 import lombok.Data;
 
 @Data
@@ -13,4 +15,5 @@ public class UserDetailForm {
 	private Date birthday;
 	private Integer age;
 	private Integer gender;
+	private Department department;
 }
