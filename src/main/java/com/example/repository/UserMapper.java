691dd1304d26217a102870174aeffabd19f509b4
LAPTOP-JG661I0R\tomoyuki <hzd02765g@gmail.com>
2022-09-18 16:43:34 +0900

    8-3-1.動的SQL

--- src/main/java/com/example/repository/UserMapper.java
+++ src/main/java/com/example/repository/UserMapper.java
@@ -10,13 +10,18 @@ import com.example.domain.user.model.MUser;
 @Mapper
 public interface UserMapper {
 
+	/** ユーザー登録 */
 	public int insertOne(MUser user);
 	
-	public List<MUser> findMany();
+	/** ユーザー取得 */
+	public List<MUser> findMany(MUser user);
 	
+	/** ユーザー取得（1件） */
 	public MUser findOne(String userId);
 	
+	/** ユーザー更新（1件） */
 	public void updateOne(@Param("userId") String userId, @Param("password") String password, @Param("userName") String userName);
 	
+	/** ユーザー削除（1件） */
 	public int deleteOne(@Param("userId") String userId);
 }
