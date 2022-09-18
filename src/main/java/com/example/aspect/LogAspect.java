a0c81022578aa64e7ec572495c362239c54714cf
LAPTOP-JG661I0R\tomoyuki <hzd02765g@gmail.com>
2022-09-18 18:14:21 +0900

    9-2-2.BeforeAfterの実装

---
+++ src/main/java/com/example/aspect/LogAspect.java
@@ -0,0 +1,25 @@
+package com.example.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+
+	@Before("execution(* *..*.*UserService.*(..))")
+	public void startLog(JoinPoint jp) {
+		log.info("メソッド開始：" + jp.getSignature());
+	}
+	
+	@After("execution(* *..*.*UserService.*(..))")
+	public void endLog(JoinPoint jp) {
+		log.info("メソッド終了：" + jp.getSignature());
+	}
+}
