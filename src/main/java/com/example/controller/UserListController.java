25605ea654db12aff519aa8d3331f77c8dcc18f6
LAPTOP-JG661I0R\tomoyuki <hzd02765g@gmail.com>
2022-09-18 13:55:34 +0900

    8-2-2.select(複数件)

--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -1,15 +1,30 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController {
+	
+	@Autowired
+	private UserService userService;
 
+	/** ユーザー一覧画面を表示 */
 	@GetMapping("/list")
-	public String GetUserList() {
+	public String getUserList(Model model) {
+		List<MUser> userList = userService.getUsers();
+		
+		model.addAttribute("userList", userList);
+		
 		return "user/list";
 	}
 }
--- src/main/java/com/example/domain/user/service/UserService.java
+++ src/main/java/com/example/domain/user/service/UserService.java
@@ -1,7 +1,11 @@
 package com.example.domain.user.service;
 
+import java.util.List;
+
 import com.example.domain.user.model.MUser;
 
 public interface UserService {
 	public void signup(MUser user);
+	
+	public List<MUser> getUsers();
 }
