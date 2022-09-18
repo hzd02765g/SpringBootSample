11573bf190f409fa5e003df92b382df79beca213
LAPTOP-JG661I0R\tomoyuki <hzd02765g@gmail.com>
2022-09-18 18:22:10 +0900

    9-2-3.Aroundの実装

--- src/main/java/com/example/aspect/LogAspect.java
+++ src/main/java/com/example/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.example.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -21,5 +23,20 @@ public class LogAspect {
 	@After("execution(* *..*.*UserService.*(..))")
 	public void endLog(JoinPoint jp) {
 		log.info("メソッド終了：" + jp.getSignature());
+	}
+	
+	@Around("@within(org.springframework.stereotype.Controller)")
+	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+		log.info("メソッド開始：" + jp.getSignature());
+		try {
+			Object result = jp.proceed();
+			log.info("メソッド終了：" + jp.getSignature());
+			return result;
+			
+		} catch(Exception e) {
+			log.error("メソッド異常終了：" + jp.getSignature());
+			
+			throw e;
+		}
 	}
 }
